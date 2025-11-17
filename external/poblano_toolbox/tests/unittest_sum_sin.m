644c4ad31ad92ef215aaab4c25ad620330a972b9
Geert Roelof van der Ploeg <g.r.ploeg@uva.nl>
2025-11-17 12:48:05 +0100

    Remove external tests

--- external/poblano_toolbox/tests/unittest_sum_sin.m
+++
@@ -1,8 +0,0 @@
-% fixture: function
-function [f,g] = sum_sin(x,a)
-
-if nargin < 2, a = 1; end
-f = sum(sin(a*x));
-g = a*cos(a*x);
-
-end
--- external/poblano_toolbox/tests/unittest_sum_squares.m
+++
@@ -1,6 +0,0 @@
-function [f,g] = unittest_function_1(x)
-
-f = 0.5 * sum(x.^2);
-g = x;
-
-end
