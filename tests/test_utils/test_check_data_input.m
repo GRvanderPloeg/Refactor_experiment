efed0166a9a3e5995e500ecec6411a116f162866
Geert Roelof van der Ploeg <g.r.ploeg@uva.nl>
2025-11-17 14:37:47 +0100

    Create test_check_data_input.m

---
+++ tests/test_utils/test_check_data_input.m
@@ -0,0 +1,233 @@
+function tests = test_check_data_input
+    tests = functiontests(localfunctions);
+end
+
+function test_matrix(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    sz     = {60,50};
+    modes  = {[1 2]};
+    lambdas_data= {[1 1 1]};
+
+    coupling.lin_coupled_modes = [0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(2,1);
+
+    loss_function{1} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    sz     = {60,50,70};
+    modes  = {[1 2 3]};
+    lambdas_data= {[1 1 1]};
+
+    coupling.lin_coupled_modes = [0 0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(3,1);
+
+    loss_function{1} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_regular_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'PAR2';
+    sz     = {40,120*ones(1,60),60};
+    modes  = {[1 2 3]};
+    lambdas_data= {[1 1 1]};
+
+    coupling.lin_coupled_modes = [0 0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(3,1);
+
+    loss_function{1} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_irregular_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'PAR2';
+    sz     = {40,[61:1:120],60};
+    modes  = {[1 2 3]};
+    lambdas_data= {[1 1 1]};
+
+    coupling.lin_coupled_modes = [0 0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(3,1);
+
+    loss_function{1} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_matrix_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {20,30, 20,30,20};
+    modes  = {[1 2], [3 4 5]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    coupling.lin_coupled_modes = [1 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(5,1);
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_CP_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {20,30,40, 20,30,20};
+    modes  = {[1 2 3], [4 5 6]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+
+    coupling.lin_coupled_modes = [1 0 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(6,1);
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_matrix_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'PAR2';
+    sz     = {40,60, 40,120*ones(1,50),50};
+    modes  = {[1 2], [3 4 5]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+
+    coupling.lin_coupled_modes = [1 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(5,1);
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_CP_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'PAR2';
+    sz     = {20,30,40, 20,30*ones(1,20),20};
+    modes  = {[1 2 3], [4 5 6]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+
+    coupling.lin_coupled_modes = [1 0 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(6,1);
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_matrix_CP_partialcoupling(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {50,30,40,50,70};
+    modes  = {[1 2 3], [4 5]};
+    lambdas_data= {[1 1 1 1], [1 1 1]};
+
+    coupling.lin_coupled_modes = [1 0 0 1 0];
+    coupling.coupling_type = 4;
+    coupling.coupl_trafo_matrices = cell(5,1);
+    coupling.coupl_trafo_matrices{1} = eye(4,4);
+    coupling.coupl_trafo_matrices{4} = [eye(3,3);0 0 0];
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_CP_CP_doublesamplingrate(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {50,30,40,100,70,80};
+    modes  = {[1 2 3], [4 5 6]};
+    lambdas_data= {[1 1 1 1], [1 1 1 1]};
+
+    coupling.lin_coupled_modes = [1 0 0 1 0 0];
+    coupling.coupling_type = 1;
+    coupling.coupl_trafo_matrices = cell(6,1);
+    coupling.coupl_trafo_matrices{1} = eye(50,50);
+    coupling.coupl_trafo_matrices{4} = zeros(50,100);
+    for i=1:50
+      coupling.coupl_trafo_matrices{4}(i,i+(i-1)) = 1; %take every second entry
+    end
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_matrix_matrix_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    model{3} = 'CP';
+    sz     = {50,60,40,50,70,60,80};
+    modes  = {[1 2 3], [4 5],[6 7]};
+    lambdas_data= {[1 1 1], [1 1 1], [1 1 1]};
+
+    coupling.lin_coupled_modes = [1 2 0 1 0 2 0];
+    coupling.coupling_type = [0 0]; 
+    coupling.coupl_trafo_matrices = cell(7,1);
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function{3} = 'Frobenius';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
+
+function test_matrix_CP_Poisson(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {50,30,40,50,70};
+    modes  = {[1 2 3], [4 5]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    shape = 1;
+    scale = 1;
+
+    coupling.lin_coupled_modes = [1 0 0 1 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(5,1);
+
+    loss_function{1} = 'KL';
+    loss_function{2} = 'KL';
+
+    cmtf.utils.check_data_input(sz,modes,lambdas_data,coupling,loss_function,model);
+end
