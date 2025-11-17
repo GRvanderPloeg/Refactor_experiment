8563adfb53d0abe799b6277a08204af6646332e5
Geert Roelof van der Ploeg <g.r.ploeg@uva.nl>
2025-11-17 15:37:27 +0100

    Create test_init_coupled_AOADMM_CMTF.m

---
+++ tests/test_utils/test_init_coupled_AOADMM_CMTF.m
@@ -0,0 +1,602 @@
+function tests = test_init_coupled_AOADMM_CMTF
+    tests = functiontests(localfunctions);
+end
+
+function test_matrix(testCase)
+    rng(0,'twister');
+
+    P = 1;
+    model{1} = 'CP';
+    sz     = {60,50};
+    modes  = {[1 2]};
+    lambdas_data= {[1 1 1]};
+    noise = 0.8;
+
+    coupling.lin_coupled_modes = [0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(2,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) randn(x,y), @(x,y) randn(x,y)};
+
+    loss_function{1} = 'Frobenius';
+    loss_function_param{1} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    sz     = {60,50,70};
+    modes  = {[1 2 3]};
+    lambdas_data= {[1 1 1]};
+    noise = 0.8;
+
+    coupling.lin_coupled_modes = [0 0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(3,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) randn(x,y), @(x,y) randn(x,y),@(x,y) randn(x,y)};
+
+    loss_function{1} = 'Frobenius';    
+    loss_function_param{1} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_regular_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'PAR2';
+    sz     = {40,120*ones(1,60),60};
+    modes  = {[1 2 3]};
+    lambdas_data= {[1 1 1]};
+    noise = 0.2;
+
+    coupling.lin_coupled_modes = [0 0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(3,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y), @(x,y) rand(x,y),@(x,y) rand(x,y)+0.1};
+
+    loss_function{1} = 'Frobenius';
+    loss_function_param{1} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_irregular_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'PAR2';
+    sz     = {40,[61:1:120],60};
+    modes  = {[1 2 3]};
+    lambdas_data= {[1 1 1]};
+    noise = 0.2;
+
+    coupling.lin_coupled_modes = [0 0 0];
+    coupling.coupling_type = [];
+    coupling.coupl_trafo_matrices = cell(3,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) randn(x,y), @(x,y) randn(x,y),@(x,y) rand(x,y)+0.1};
+
+    loss_function{1} = 'Frobenius';
+    loss_function_param{1} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_matrix_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {20,30, 20,30,20};
+    modes  = {[1 2], [3 4 5]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    noise = 0;
+
+    coupling.lin_coupled_modes = [1 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(5,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y),@(x,y) randn(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y)};
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function_param{1} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_CP_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {20,30,40, 20,30,20};
+    modes  = {[1 2 3], [4 5 6]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    noise = 0;
+
+    coupling.lin_coupled_modes = [1 0 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(6,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y), @(x,y) randn(x,y),@(x,y) randn(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y)};
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_matrix_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'PAR2';
+    sz     = {40,60, 40,120*ones(1,50),50};
+    modes  = {[1 2], [3 4 5]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    noise = 0.5;
+
+    coupling.lin_coupled_modes = [1 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(5,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y), @(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y)+0.1}; % function handle of distribution of data within each factor matrix /or Delta if linearly coupled, x,y are the size inputs %coupled modes need to have same distribution! If not, just the first one will be considered
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_CP_PAR2(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'PAR2';
+    sz     = {20,30,40, 20,30*ones(1,20),20};
+    modes  = {[1 2 3], [4 5 6]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    noise = 0;
+
+    coupling.lin_coupled_modes = [1 0 0 1 0 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(6,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y), @(x,y) randn(x,y),@(x,y) randn(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y)+0.1};
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_matrix_CP_partialcoupling(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {50,30,40,50,70};
+    modes  = {[1 2 3], [4 5]};
+    lambdas_data= {[1 1 1 1], [1 1 1]};
+    noise = 0.2;
+
+    coupling.lin_coupled_modes = [1 0 0 1 0];
+    coupling.coupling_type = 4;
+    coupling.coupl_trafo_matrices = cell(5,1);
+    coupling.coupl_trafo_matrices{1} = eye(4,4);
+    coupling.coupl_trafo_matrices{4} = [eye(3,3);0 0 0];
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y), @(x,y) randn(x,y),@(x,y) randn(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y)};
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_CP_CP_doublesamplingrate(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {50,30,40,100,70,80};
+    modes  = {[1 2 3], [4 5 6]};
+    lambdas_data= {[1 1 1 1], [1 1 1 1]};
+    noise = 0.2;
+
+    coupling.lin_coupled_modes = [1 0 0 1 0 0];
+    coupling.coupling_type = 1;
+    coupling.coupl_trafo_matrices = cell(6,1);
+    coupling.coupl_trafo_matrices{1} = eye(50,50);
+    coupling.coupl_trafo_matrices{4} = zeros(50,100);
+    for i=1:50
+      coupling.coupl_trafo_matrices{4}(i,i+(i-1)) = 1; %take every second entry
+    end
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) randn(x,y),@(x,y) randn(x,y), @(x,y) randn(x,y),@(x,y) randn(x,y),@(x,y) randn(x,y),@(x,y) rand(x,y)};
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_matrix_matrix_CP(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    model{3} = 'CP';
+    sz     = {50,60,40,50,70,60,80};
+    modes  = {[1 2 3], [4 5],[6 7]};
+    lambdas_data= {[1 1 1], [1 1 1], [1 1 1]};
+    noise = 0.2;
+
+    coupling.lin_coupled_modes = [1 2 0 1 0 2 0];
+    coupling.coupling_type = [0 0]; 
+    coupling.coupl_trafo_matrices = cell(7,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) rand(x,y), @(x,y) rand(x,y),@(x,y) randn(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y),@(x,y) rand(x,y)};
+
+    loss_function{1} = 'Frobenius';
+    loss_function{2} = 'Frobenius';
+    loss_function{3} = 'Frobenius';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+    loss_function_param{3} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
+
+function test_matrix_CP_Poisson(testCase)
+    rng(0,'twister');
+
+    model{1} = 'CP';
+    model{2} = 'CP';
+    sz     = {50,30,40,50,70};
+    modes  = {[1 2 3], [4 5]};
+    lambdas_data= {[1 1 1], [1 1 1]};
+    noise = 0.2;
+    shape = 1;
+    scale = 1;
+
+    coupling.lin_coupled_modes = [1 0 0 1 0];
+    coupling.coupling_type = 0;
+    coupling.coupl_trafo_matrices = cell(5,1);
+    
+    normalize_columns = 0;
+    distr_data = {@(x,y) gamrnd(shape,scale,x,y),@(x,y) gamrnd(shape,scale,x,y),@(x,y) gamrnd(shape,scale,x,y),@(x,y) gamrnd(shape,scale,x,y),@(x,y) gamrnd(shape,scale,x,y)};
+
+    loss_function{1} = 'KL';
+    loss_function{2} = 'KL';
+    loss_function_param{1} = [];
+    loss_function_param{2} = [];
+
+    constrained_modes = [0 0 0];
+    constraints = cell(length(constrained_modes),1);
+    weights = [1/2 1/2];
+
+    Z.loss_function = loss_function;
+    Z.loss_function_param = loss_function_param;
+    Z.model = model;
+    Z.modes = modes;
+    Z.size  = sz;
+    Z.coupling = coupling;
+    Z.constrained_modes = constrained_modes;
+    Z.constraints = constraints;
+    Z.weights = weights;
+
+    init_options.lambdas_init = lambdas_data;
+    init_options.nvecs = 0;
+    init_options.distr = distr_data;
+    init_options.normalize = 1;
+    
+    [X, ~, ~,~] = cmtf.utils.create_coupled_data('model', model, 'size', sz, 'modes', modes, 'lambdas', lambdas_data, 'noise', noise,'coupling',coupling,'normalize_columns',normalize_columns,'distr_data',distr_data,'loss_function',Z.loss_function); %create data
+
+    for p=1:P
+        Z.object{p} = X{p};
+    end
+
+    cmtf.utils.init_coupled_AOADMM_CMTF(Z,'init_options', init_options);
+end
