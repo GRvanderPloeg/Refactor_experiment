1a32cdaa1ddfc3e7cb9d379141df11fc454cb8ed
Geert Roelof van der Ploeg <g.r.ploeg@uva.nl>
2025-12-02 12:52:57 +0100

    Rename of tests + script1 assertion implemented

--- tests/test_aoadmm/test_script1_CP_PAR2_nonneg.m
+++ tests/test_aoadmm/test_script1_CP_PAR2_nonneg.m
@@ -135,4 +135,7 @@ function test_workflow(testCase)
     end
     FMS2_B = score(ktensor(ones(3,1),SollargeB),ktensor(ones(3,1),largeB),'lambda_penalty',false);
 
+    % See 
+    verifyTrue(testCase, FMS1 >= 0.99 & FMS2A >= 0.99 & FMS2_B >= 0.99 & FMS2_C >= 0.99)
+
 end
