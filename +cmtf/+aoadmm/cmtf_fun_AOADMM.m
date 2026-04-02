3e5e096198ed57099ae398960d571f9394c0c0ec
Geert Roelof van der Ploeg <g.r.ploeg@uva.nl>
2026-04-02 15:34:30 +0200

    New: EM for missing data

--- +cmtf/+aoadmm/cmtf_fun_AOADMM.m
+++ +cmtf/+aoadmm/cmtf_fun_AOADMM.m
@@ -40,6 +40,8 @@ function [G,out] = cmtf_fun_AOADMM(Z,Znorm_const, G,fh,gh,lscalar,uscalar,option
         which_p(i) = find(cellfun(@(x) any(ismember(x,i)),Z.modes));
     end
     P = numel(Z.object);
+    has_missing = isfield(Z, 'miss') && any(~cellfun(@isempty, Z.miss));
+    f_rel_missing = Inf;
     G_transp_G = cell(nb_modes,1);
     A = cell(nb_modes,1);
     C = cell(nb_modes,1);
@@ -60,12 +62,20 @@ function [G,out] = cmtf_fun_AOADMM(Z,Znorm_const, G,fh,gh,lscalar,uscalar,option
     time_at_it(1) = 0;
     %display first iteration
     if strcmp(options.Display,'iter') || strcmp(options.Display,'final')
-        fprintf(1,' Iter  f total      f tensors      f couplings    f constraints    f PAR2 couplings\n');
+        if has_missing
+            fprintf(1,' Iter  f total      f tensors      f couplings    f constraints    f PAR2 couplings  f_rel_miss\n');
+        else
+            fprintf(1,' Iter  f total      f tensors      f couplings    f constraints    f PAR2 couplings\n');
+        end
         fprintf(1,'------ ------------ -------------  -------------- ---------------- ----------------\n');
     end
 
     if strcmp(options.Display,'iter')
-        fprintf(1,'%6d %12f %12f %12f %17f %12f\n', 0, f_total, f_tensors, f_couplings,f_constraints, f_PAR2_couplings);
+        if has_missing
+            fprintf(1,'%6d %12f %12f %12f %17f %12f %12f\n', 0, f_total, f_tensors, f_couplings, f_constraints, f_PAR2_couplings, f_rel_missing);
+        else
+            fprintf(1,'%6d %12f %12f %12f %17f %12f\n', 0, f_total, f_tensors, f_couplings,f_constraints, f_PAR2_couplings);
+        end
     end
     iter = 1;
     
@@ -192,8 +202,58 @@ function [G,out] = cmtf_fun_AOADMM(Z,Znorm_const, G,fh,gh,lscalar,uscalar,option
             end
             %end
         end % loop over coupling groups
-        
-        
+
+        % EM imputation: update missing entries with the current low-rank model.
+        % After this step, Z.object{p}(missing) == model(missing), so the
+        % Frobenius residual at missing positions is exactly zero and the
+        % objective measures fit on observed entries only.
+        if has_missing
+            num_sq = 0;
+            den_sq = 0;
+            for p = 1:P
+                if isempty(Z.miss{p}), continue; end
+                if strcmp(Z.model{p}, 'CP')
+                    M_full = double(full(ktensor(G.fac(Z.modes{p}))));
+                    miss_mask = ~Z.miss{p};
+                    old_vals = double(Z.object{p}(miss_mask));
+                    new_vals = M_full(miss_mask);
+                    num_sq = num_sq + sum((new_vals - old_vals).^2);
+                    den_sq = den_sq + sum(old_vals.^2);
+                    if isa(Z.object{p}, 'tensor')
+                        tmp = double(Z.object{p});
+                        tmp(miss_mask) = new_vals;
+                        Z.object{p} = tensor(tmp);
+                        Znorm_const{p} = norm(Z.object{p})^2;
+                    else
+                        Z.object{p}(miss_mask) = new_vals;
+                        Znorm_const{p} = norm(Z.object{p}(:))^2;
+                    end
+                elseif strcmp(Z.model{p}, 'PAR2')
+                    m1 = Z.modes{p}(1); m2 = Z.modes{p}(2); m3 = Z.modes{p}(3);
+                    Znorm_const{p} = 0;
+                    for k = 1:length(Z.object{p})
+                        M_k = G.fac{m1} * diag(G.fac{m3}(k,:)) * G.fac{m2}{k}';
+                        miss_k = ~Z.miss{p}{k};
+                        old_k = Z.object{p}{k}(miss_k);
+                        new_k = M_k(miss_k);
+                        num_sq = num_sq + sum((new_k - old_k).^2);
+                        den_sq = den_sq + sum(old_k.^2);
+                        Z.object{p}{k}(miss_k) = new_k;
+                        Znorm_const{p} = Znorm_const{p} + norm(Z.object{p}{k}, 'fro')^2;
+                    end
+                end
+                % Invalidate the per-tensor MTTKRP cache so func_eval uses the
+                % slow path (recomputes from the updated Z.object{p}).
+                last_mttkrp{p} = [];
+                last_had{p} = [];
+            end
+            if den_sq > 0
+                f_rel_missing = sqrt(num_sq / den_sq);
+            else
+                f_rel_missing = sqrt(num_sq);
+            end
+        end
+
        f_tensors_old = f_tensors;
        f_couplings_old = f_couplings;
        f_constraints_old = f_constraints;
@@ -206,6 +266,9 @@ function [G,out] = cmtf_fun_AOADMM(Z,Znorm_const, G,fh,gh,lscalar,uscalar,option
        func_PAR2_coupl(iter+1) = f_PAR2_couplings;
        time_at_it(iter+1) = toc(tstart);
        stop = cmtf.utils.evaluate_stopping_conditions(f_tensors,f_couplings,f_constraints,f_PAR2_couplings,f_tensors_old,f_couplings_old,f_constraints_old,f_PAR2_couplings_old,options);
+       if has_missing
+           stop = stop && (f_rel_missing < options.OuterRelTol);
+       end
 
         %display
         if strcmp(options.Display,'iter') && mod(iter,options.DisplayIters)==0
